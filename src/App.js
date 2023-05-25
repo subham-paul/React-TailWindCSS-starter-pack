b8eb858de0c34a81a239e67ad5df6b39b3349cc2
subham-paul <subhampaul456@gmail.com>
2023-05-25 09:51:00 +0530

    starter pack

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+export function App() {
+  return (
+    <h1 className="text-center text-lg font-bold underline">
+      This is a React TailWind-CSS starter pack!
+    </h1>
+  )
+}
+
+export default App;
